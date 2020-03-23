257970023c4b55d190a0f9222f91014caf7567d1
tszabo <tamas@tszabo.ro>
2020-03-23 07:58:27 +0100

    Update application
    * Add the SavitzkyGolay filter to smooth the imu data and calculate its derivative
    * Restructure the code into three functions that run at different rates

--- arduino/main.cpp
+++ arduino/main.cpp
@@ -1,9 +1,12 @@
 
 #include <Arduino.h>
+
 #include "include/MPU6050Wrapper.h"
+#include "include/Motor.h"
+#include "include/primitives/ring_buffer.h"
+#include "include/primitives/array.h"
 
-
-MPU6050Wrapper mpu(2);
+#include "include/filters/savitzky_golay.h"
 
 float getSupplyVoltage()
 {
@@ -13,12 +16,15 @@ float getSupplyVoltage()
   return (static_cast<float>(ref)/1023.0) * shunt_multiplier * ref_voltage;
 }
 
-unsigned long last_print_time = 0;
+
+MPU6050Wrapper mpu(2);
+
+Motor left_motor(6, 9, 254, 0);
+Motor right_motor(10, 11, 254, 0);
+
 void setup()
 {
-  // a/g:	18400	10398	7522	1135	267	-211
   Serial.begin(115200);
-
   mpu.initialize( -59,    // gyro_x_offset
                   9,      // gyro_y_offset
                   -30,    // gyro_z_offset
@@ -27,35 +33,81 @@ void setup()
                   927);   // acc_z_offset
 
   pinMode(A0, INPUT);
-  last_print_time = millis();
 }
 
+float angle_ref = 0; // At some point this is going to be the control variable for the speed!
 
-void loop()
+
+constexpr unsigned int fast_loop_rate_ms = 5;
+constexpr unsigned int slow_loop_rate_ms = 25;
+constexpr unsigned int comm_loop_rate_ms = 100;
+
+unsigned long last_fast_exec = 0;
+unsigned long last_slow_exec = 0;
+unsigned long last_comm_exec = 0;
+
+
+SavitzkyGolayFilter<double, 5, 3, 0> imu_filter(0.01); // This is the rate with which the IMU provides data
+
+void fastLoop(unsigned long T)
 {
-  if (!mpu.read())
+  if (T < last_fast_exec + fast_loop_rate_ms)
   {
     return;
   }
+  last_fast_exec  = T;
 
-  float roll  = mpu.getRoll();
-  float pitch = mpu.getPitch();
-  float yaw   = mpu.getYaw();
-
-  float voltage = getSupplyVoltage();
-
-  unsigned long now = millis();
-
-  if (now - last_print_time > 20)
+  if (mpu.read())
   {
-    Serial.print("ypr\t");
-    Serial.print(yaw * 180/M_PI);
-    Serial.print("\t");
-    Serial.print(pitch * 180/M_PI);
-    Serial.print("\t");
-    Serial.print(roll * 180/M_PI);
-    Serial.print("\t - \t");
-    Serial.print(voltage);
-    Serial.println("");
+    imu_filter.push(angle_ref - mpu.getRoll());
   }
+}
+
+float kP = 1;
+float kD = 1;
+
+void slowLoop(unsigned long T)
+{
+  if (T < last_slow_exec + slow_loop_rate_ms)
+  {
+    return;
+  }
+  last_slow_exec  = T;
+
+
+  double err = imu_filter.filter(0);
+  double err_diff = imu_filter.filter(1);
+
+  float cmd = (-1) * (kP * err + kD * err_diff);
+  auto left_setspeed = left_motor.setSpeed(cmd);
+  auto right_setspeed = right_motor.setSpeed(cmd);
+
+  Serial.print(err * 10);
+  Serial.print(" ");
+  Serial.print(err_diff * 10);
+  Serial.print(" ");
+  Serial.print(cmd * 10);
+  Serial.print(" ");
+  Serial.print(static_cast<float>(left_setspeed)*10/255);
+  Serial.print(" ");
+  Serial.print(static_cast<float>(right_setspeed)*10/255);
+
+  Serial.println("");
+}
+
+void commLoop(unsigned long T)
+{
+  if (T < last_comm_exec + comm_loop_rate_ms)
+  {
+    return;
+  }
+  last_comm_exec  = T;
+}
+
+void loop()
+{
+  unsigned long now = millis();
+  fastLoop(now);
+  slowLoop(now);
+  commLoop(now);
 }
